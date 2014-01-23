e2226372a4ea94336313cd231e484a4ca8b0f562
Manuel Escobar <manuelescobar@Manuels-MacBook-Pro.local>
2014-01-23 17:35:48 -0600

    Initial Commit

---
+++ tarea_1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  tarea_1
+//
+//  Created by Manuel Escobar on 1/23/14.
+//  Copyright (c) 2014 Manuel Escobar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
