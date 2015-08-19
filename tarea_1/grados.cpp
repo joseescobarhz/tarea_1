4343635a4cfb0bca936fb9b80ec5bf73759ec7d1
jose.escbarhz <j.manuel_e@hotmail.com>
2015-08-19 12:48:16 -0500

    /Users/manuelescobar/Desktop/Paradigmas/hello.js

--- tarea_1/grados.cpp
+++ tarea_1/grados.cpp
@@ -3,19 +3,17 @@ using namespace std;
 
 int main()
 {
-    // Declaración de variables
     double gradc,gradf;
-    
-    //Solicita los grados a transformar
+
     cout<<"\tGrados Farenheit a convertir:  ";
     cin>>gradf;
     
-    //Realiza la transformación
-    gradc=(gradf-32)*9/5;
+    gradc=(gradf-32)*5/9;
     
-    //Muestra el resultado
-    cout<<"Son equivalentes a  "<< gradc<< "  grados Centigrados " <<endl;
+    cout<<endl<<gradf<<" grados farenhet es equivalentes a  "<< gradc<< "  grados Centigrados " <<endl;
     getchar();
-    getchar();
+    
+    return 0;
 }
+
 
